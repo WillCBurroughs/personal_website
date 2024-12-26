b9c11eb77b420a2ec842400bb05e4bd7359f5f6d
WillCBurroughs <willcburroughs@gmail.com>
2024-12-26 17:32:16 -0600

    Adding third blur

--- src/components/molecules/ContactMeDesktop.js
+++ src/components/molecules/ContactMeDesktop.js
@@ -5,7 +5,10 @@ import BlueBlot from "../atoms/BlueBlot";
 function ContactMeDesktop() {
     return (
         <div className="contactMeHolder">
+            {/* Three Blurs on page */}
             <BlueBlot id={"blueBlot1"} width={60} top={390} left={22} />
+            <BlueBlot id={"blueBlot2"} width={50} top={330} left={-35} />
+            <BlueBlot id={"blueBlot3"} width={50} top={320} left={85} />
         </div>
     );
 }
