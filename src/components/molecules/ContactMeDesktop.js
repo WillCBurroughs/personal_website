b2d05f2d214c36b74e0b1e7b88dba9654d43f591
WillCBurroughs <willcburroughs@gmail.com>
2024-12-27 19:02:52 -0600

    Bug Fix: Correcting div being too wide issue

--- src/components/molecules/ContactMeDesktop.js
+++ src/components/molecules/ContactMeDesktop.js
@@ -6,9 +6,14 @@ function ContactMeDesktop() {
     return (
         <div className="contactMeHolder">
             {/* Three Blurs on page */}
-            <BlueBlot id={"blueBlot1"} width={60} top={390} left={22} />
-            <BlueBlot id={"blueBlot2"} width={50} top={330} left={-35} />
-            <BlueBlot id={"blueBlot3"} width={50} top={320} left={85} />
+            <div className="blurHolder">
+                <BlueBlot id={"blueBlot1"} width={60} top={65} left={20} />
+                <BlueBlot id={"blueBlot2"} width={50} top={10} left={-35} />
+                <BlueBlot id={"blueBlot3"} width={50} top={0} left={80} />
+            </div>
+            <div className="holdForm">
+
+            </div>
         </div>
     );
 }
