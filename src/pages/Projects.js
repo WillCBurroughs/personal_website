9ecac0694e9ddf87b7fc30653c6c542824bd69de
WillCBurroughs <willcburroughs@gmail.com>
2025-01-21 17:03:26 -0500

    Feature development: positioning mouseDown and adding top projects section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,26 @@
 import React from "react";
 import NavBar from "../components/organisms/NavBar";
+import HomeHeroRight from "../components/molecules/HomeHeroRight";
+import HomeHeroLeft from "../components/molecules/HomeHeroLeft";
 import '../App.css';
+import '../index.css';
+import ShadowBlur from "../components/atoms/ShadowBlur";
+import MouseDownHeader from "../components/atoms/MouseDownHeader";
 
 
 
 export default function Projects() {
   return (
-
-    <div className="App">
-      <header className="App-header">
+    <>
+    <div className="homeHero">
       <NavBar/>
-
-      </header>
+      <div className="homeHeroContainer">
+        <HomeHeroLeft />
+        <HomeHeroRight />
+      </div>
+      <ShadowBlur width = {1600}/>
+      <MouseDownHeader/>
     </div>
-    
+    </>
   );
 }
