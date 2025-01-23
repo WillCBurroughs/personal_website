27c88bbe3eeeefc3e7ed012bdf960e0e4357a199
WillCBurroughs <willcburroughs@gmail.com>
2025-01-23 17:51:08 -0500

    First version of contact page desktop

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,22 @@
 import React from "react";
 import NavBar from "../components/organisms/NavBar";
 import '../App.css';
-
-
+import '../index.css';
+import ContactMeDesktopContactPage from "../components/molecules/ContactMeDesktopContactPage";
+import FooterDesktop from "../components/molecules/FooterDesktop";
 
 export default function Contact() {
   return (
-
-    <div className="App">
-      <header className="App-header">
+    <>
+    <div className="">
+      <header className="">
       <NavBar/>
 
       </header>
     </div>
+    <ContactMeDesktopContactPage/> 
+    <FooterDesktop/>
+    </>
     
   );
 }
